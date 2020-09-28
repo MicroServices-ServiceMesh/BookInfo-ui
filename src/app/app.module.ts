82cd62abe19452797af99433140101ad40219397
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-09-28 10:37:18 -0500

    updated for keycloak login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './components/log-in/log-in.component';
@@ -51,6 +51,7 @@ function initializeKeycloak(keycloak: KeycloakService) {
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     KeycloakAngularModule
   ],
 
---
+++ src/app/service/studentapi/student.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private readonly URL = 'http://localhost:8080/v1/student/list';
+
+  constructor(private http: HttpClient) {}
+
+  // create a method named: resolveItems()
+  // this method returns list-of-items in form of Observable
+  // every HTTTP call returns Observable object
+  resolveItems(): Observable<any> {
+    console.log('Request is sent!');
+    // this.http is a HttpClient library provide by @angular/common
+    // we are calling .get() method over this.http object
+    //this .get() method takes URL to call API
+    return this.http.get(this.URL);
+  }
+}
